c5cc136434be9cda8ba5d0743d8b061c49874a98
jacobmorency <jvmorency@gmail.com>
2025-06-28 19:25:06 -0400

    Update fixed bug with top rated tv shows and movies

--- bingelist/src/app/catalog/page.tsx
+++ bingelist/src/app/catalog/page.tsx
@@ -1,11 +1,22 @@
 import { fetchTMDB } from "@/lib/tmdb";
 import CatalogContent from "@/app/components/catalog/catalogcontent";
+import type { MediaResult } from "@/types/movie";
 
 export default async function CatalogPage() {
   const trendingMovies = (await fetchTMDB("trending/movie/week")).results;
   const trendingTV = (await fetchTMDB("trending/tv/week")).results;
-  const topRatedMovies = (await fetchTMDB("movie/top_rated")).results;
-  const topRatedTV = (await fetchTMDB("tv/top_rated")).results;
+  const topRatedMoviesRaw = (await fetchTMDB("movie/top_rated")).results;
+  const topRatedTVRaw = (await fetchTMDB("tv/top_rated")).results;
+
+  const topRatedMovies = topRatedMoviesRaw.map((item: MediaResult) => ({
+    ...item,
+    media_type: "movie",
+  }));
+
+  const topRatedTV = topRatedTVRaw.map((item: MediaResult) => ({
+    ...item,
+    media_type: "tv",
+  }));
 
   return (
     <CatalogContent
