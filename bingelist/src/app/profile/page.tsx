f7253eca18590a693aa0ef08992b8310cfe8e4c9
jacobmorency <jvmorency@gmail.com>
2025-06-29 19:51:18 -0400

    Update created simple profile page

--- bingelist/src/app/profile/page.tsx
+++ bingelist/src/app/profile/page.tsx
@@ -2,14 +2,25 @@
 import ClientLayout from "@/app/components/clientlayout";
 import { useAuth } from "@/app/context/authContext";
 import ProfileStatBox from "@/app/components/profile/profilestatbox";
+import ProfileCard from "@/app/components/profile/profilecard";
 import supabase from "@/app/lib/supabaseClient";
 import { useState, useEffect } from "react";
+import { formatDistanceToNow, isToday } from "date-fns";
 
 export default function ProfilePage() {
-  const [watchedCount, setWatchedCount] = useState(0);
-  const [toWatchCount, setToWatchCount] = useState(0);
-  const [favoritesCount, setFavoritesCount] = useState(0);
-  // const [tbdCount, setTbdCount] = useState(0);
+  const [watchedCount, setWatchedCount] = useState<number>(0);
+  const [toWatchCount, setToWatchCount] = useState<number>(0);
+  const [favoritesCount, setFavoritesCount] = useState<number>(0);
+  // const [tbdCount, setTbdCount] = useState<number>(0);
+  type WatchedMovie = {
+    id: string;
+    user_id: string;
+    title: string;
+    watched_at: string;
+  };
+
+  const [lastActivity, setLastActivity] = useState<WatchedMovie | null>(null);
+
   const { user, userData } = useAuth();
 
   useEffect(() => {
@@ -36,7 +47,24 @@ export default function ProfilePage() {
 
       // TBD count is not implemented in the database yet, so we set it to 0
     };
+
+    const fetchLastActivity = async () => {
+      if (!user) return;
+
+      const { data: lastActivityData } = await supabase
+        .from("watched_movies")
+        .select("*")
+        .eq("user_id", user.id)
+        .order("watched_at", { ascending: false })
+        .limit(1);
+
+      if (lastActivityData && lastActivityData.length > 0) {
+        setLastActivity(lastActivityData[0]);
+      }
+    };
+
     fetchCounts();
+    fetchLastActivity();
   }, [user]);
 
   if (!user || !userData) {
@@ -61,6 +89,21 @@ export default function ProfilePage() {
           <ProfileStatBox label="Favorites" stat={favoritesCount} />
           <ProfileStatBox label="TBD" stat={0} />
         </div>
+        {lastActivity && (
+          <div className="my-4 w-full">
+            <ProfileCard
+              title="Recent Activity"
+              content={`Watched "${lastActivity.title}"`}
+              subcontent={
+                isToday(new Date(lastActivity.watched_at))
+                  ? "Today"
+                  : `${formatDistanceToNow(new Date(lastActivity.watched_at), {
+                      addSuffix: true,
+                    })}`
+              }
+            />
+          </div>
+        )}
       </main>
     </ClientLayout>
   );
